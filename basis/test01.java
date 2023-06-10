67759df1106fbca9afdc037179273d7686e9bea6
Echous <2372566501@qq.com>
2023-06-10 18:45:07 +0800

    basis test code

---
+++ basis/test01.java
@@ -0,0 +1,15 @@
+package basis.Inner;
+
+/**
+ * @author LangJ
+ * @version 1.0
+ */
+public class test01 {
+
+    private  float f = 1.0f;
+
+     class InnerClass{
+        public float func(){return f;}
+    }
+
+}
